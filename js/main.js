e8441d8e0f4720117038764497b00a97d61c35fc
ranibling <89003387+ranibling@users.noreply.github.com>
2021-08-25 00:46:38 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ $(function(){
             $(this).removeClass('active');
             $(this).next('ul').slideUp();
         }else{
-            $('.mainParent li .withChild').removeClass('active');
+            $('.mainParent li .withChild').removeClass('active notactive');
             $('.mainParent li .withChild + ul').slideUp();
              $('.submenu a').removeClass('active')
             $(this).addClass('active');
@@ -18,13 +18,21 @@ $(function(){
         if($(this).hasClass('active')){
             $(this).removeClass('active')
         }else{  
+            $(this).parents('li').find('.withChild').addClass('active notactive')
             $('.submenu li a').removeClass('active');
             $(this).addClass('active');
         }
             $('.area').text(text);
     })
+
     $('.slideHolder').on('click', function(){
-        $(this).toggleClass('active');
+        if($(this).hasClass('active')){
+            $(this).removeClass('active');
+            $(this).text("OFF");
+        }else{
+            $(this).addClass('active');
+            $(this).text("ON");
+        }
     })
 
 
@@ -42,63 +50,8 @@ $(function(){
     function onlyUnique(value, index, self) {
         return self.indexOf(value) === index;
     }
-    function getData(){
-        $.getJSON("https://www.bling-center.com/business/inbound/calldetail/amicpa", function(data){
-            for(var i in data){
-                var dateNow = new Date(data[i].createdDate);
-                var checker = monthNames[dateNow.getMonth()]+'-'+dateNow.getDate()+'-'+ dateNow.getFullYear();
-                rawDate.push(checker);
-                rawClass.push(monthNames[dateNow.getMonth()]+dateNow.getDate());
-                uniqueDate = rawDate.filter(onlyUnique);
-                uniqueClass = rawClass.filter(onlyUnique);
-                if(uniqueDate[0] == checker){
-                dataBox = '<div class="divDetails">'+
-                        '<ul>'+
-                        '<li>'+
-                            '<span class="data">'+data[i].customerPhoneNumber+'</span>'+
-                            '<span class="legend">Phone Number</span>'+
-                        '</li>'+
-                        '<li>'+
-                            '<span class="data">'+data[i].callstatus+'</span>'+
-                            '<span class="legend">call status</span>'+
-                        '</li>'+
-                        '<li>'+
-                            '<span class="data">'+data[i].callstatus+'</span>'+
-                            '<span class="legend">call status</span>'+
-                        '</li>'+
-                        '<li>'+
-                            '<span class="data colored '+data[i].callType+'">'+ data[i].callType +'</span>'+
-                            '<span class="legend">callType</span>'+
-                        '</li>'+
-                        // '<li>'+
-                        //     '<span class="data date '+monthNames[dateNow.getMonth()]+dateNow.getDate()+'">'+ checker +'</span>'+
-                        //     '<span class="legend">callType</span>'+
-                        // '</li>'+
-                        '<li>'+
-                            '<audio controls>'+
-                                '<source src="'+data[i].recordUrl+'" type="audio/mpeg">'+
-                            ' Your browser does not support the audio element.'+
-                            ' </audio>'+
-                        '</li>'+
-                        '</ul>'+
-                    '</div>';   
-                    $('.August18').html("asdsa");
-                }
-            }   
 
-            var element = "";
-            for(var i in uniqueDate){
-                element = '<div data-class="'+uniqueClass[i]+'" class="heading">'+
-                '<h3>Calls For '+uniqueDate[i]+'</h3><a href="#" data-date='+uniqueDate[i]+' class="btnGet">Get Data</a>'+
-                '<div class="'+uniqueClass[i]+'"></div></div>';
-                $(element).appendTo('.dashboard');
-                $('.dashboard .btnGet').trigger('clickk');
-            }
-        })
-
-    }
-
-    getData();
+    // getData();
     // function getHeader(){
     //     var element = "";
     //     for(var i in uniqueDate){
@@ -111,79 +64,156 @@ $(function(){
     // }
     // getHeader();
 
-    function dataGetter(){
-        for(var j in uniqueDate){
-            $.getJSON("https://www.bling-center.com/business/inbound/calldetail/amicpa", function(data){
-                for(var i in data){
-                    var dateNow = new Date(data[i].createdDate);
-                    var checker = monthNames[dateNow.getMonth()]+'-'+dateNow.getDate()+'-'+ dateNow.getFullYear();
-                    rawDate.push(checker);
-                    rawClass.push(monthNames[dateNow.getMonth()]+dateNow.getDate());
-                    uniqueDate = rawDate.filter(onlyUnique);
-                    uniqueClass = rawClass.filter(onlyUnique);
-                }});
-                if(uniqueDate[j] == checker){
-                    var dataBox = '<span class="Phone Number"'+data[i].customerPhoneNumber+'</span>';
-                    $(dataBox).appendTo(".August17");
-                    if(uniqueDate[j] == checker){
-                        var dataBox = '<span class="Phone Number"'+data[i].customerPhoneNumber+'</span>';
-                        console.log(dataBox);
-                        $(dataBox).appendTo(".August17");
-                    }
-                }
-        }
-    }
-    dataGetter()
-    function getByDate(date,divname){
+    // function dataGetter(){
+    //     for(var j in uniqueDate){
+    //         $.getJSON("https://www.bling-center.com/business/inbound/calldetail/amicpa", function(data){
+    //             for(var i in data){
+    //                 var dateNow = new Date(data[i].createdDate);
+    //                 var checker = monthNames[dateNow.getMonth()]+'-'+dateNow.getDate()+'-'+ dateNow.getFullYear();
+    //                 rawDate.push(checker);
+    //                 rawClass.push(monthNames[dateNow.getMonth()]+dateNow.getDate());
+    //                 uniqueDate = rawDate.filter(onlyUnique);
+    //                 uniqueClass = rawClass.filter(onlyUnique);
+    //             }});
+    //             if(uniqueDate[j] == checker){
+    //                 var dataBox = '<span class="Phone Number"'+data[i].customerPhoneNumber+'</span>';
+    //                 $(dataBox).appendTo(".August17");
+    //                 if(uniqueDate[j] == checker){
+    //                     var dataBox = '<span class="Phone Number"'+data[i].customerPhoneNumber+'</span>';
+    //                     console.log(dataBox);
+    //                     $(dataBox).appendTo(".August17");
+    //                 }
+    //             }
+    //     }
+    // }
+    // dataGetter()
+    getByDate()
+    function getByDate(){
         var dataBox="";
         $.getJSON("https://www.bling-center.com/business/inbound/calldetail/amicpa", function(data){
             console.log(data)
+            var currentProcessDate ="";
             for(var i in data){
-                var dateNow = new Date(data[i].createdDate);
-                var checker = monthNames[dateNow.getMonth()]+'-'+dateNow.getDate()+'-'+ dateNow.getFullYear();
-                rawDate.push(checker);
-                rawClass.push(monthNames[dateNow.getMonth()]+dateNow.getDate());
-                uniqueDate = rawDate.filter(onlyUnique);
-                uniqueClass = rawClass.filter(onlyUnique);
-                console.log(checker+" "+date);
-                if(date==checker){
-                    dataBox = '<div class="divDetails">'+
+                var date = data[i].createdDate.split("T")[0];
+                if(currentProcessDate == "" || currentProcessDate != date){
+                    element = '<h3>Calls For '+date+'</h3>';
+                    $('.dashboard').append(element);
+                    currentProcessDate = date;
+                }
+                dataBox = '<div class="divDetails">'+
+                    '<ul>'+
+                    '<li>'+
+                        '<span class="data">'+data[i].customerPhoneNumber+'</span>'+
+                        '<span class="legend">Phone Number</span>'+
+                    '</li>'+
+                    '<li>'+
+                        '<span class="data">'+data[i].callstatus+'</span>'+
+                        '<span class="legend">call status</span>'+
+                    '</li>'+
+                    '<li>'+
+                        '<span class="data colored '+data[i].callType+'">'+ data[i].callType +'</span>'+
+                        '<span class="legend">callType</span>'+
+                    '</li>'+
+                    '<li>'+
+                        '<audio controls>'+
+                            '<source src="'+data[i].recordUrl+'" type="audio/mpeg">'+
+                        ' Your browser does not support the audio element.'+
+                        ' </audio>'+
+                    '</li>'+
+                    '</ul>'+
+                '</div>';  
+                $(dataBox).appendTo('.dashboard');  
+            }
+        })
+    }
+    $('#number').keyup(function(){
+        var number= $(this).val();
+        if(number ==""){
+            $('#dashboard').html('')
+            getByDate()
+        }
+        $('#dashboard').html('')
+        search(number);
+    });
+    function search(mobile){
+            $.getJSON("https://www.bling-center.com/business/inbound/calldetail/amicpa", function(data){
+            var currentProcessDate ="";
+            $.each(data, function(key,value){
+                var date = value.createdDate.split("T")[0];
+                var customernum = value.customerPhoneNumber;
+                if(customernum == mobile){
+                    if(currentProcessDate == "" || currentProcessDate != date){
+                        element = '<h3>Calls For '+date+'</h3>';
+                        $('.dashboard').append(element);
+                        currentProcessDate = date;
+                    }
+                    dataBox = '<div data-key="'+key+'" class="divDetails" >'+
                         '<ul>'+
                         '<li>'+
-                            '<span class="data">'+data[i].customerPhoneNumber+'</span>'+
+                            '<span class="data" data-date="'+date+'">'+value.customerPhoneNumber+'</span>'+
                             '<span class="legend">Phone Number</span>'+
                         '</li>'+
                         '<li>'+
-                            '<span class="data">'+data[i].callstatus+'</span>'+
+                            '<span class="data">'+value.callstatus+'</span>'+
                             '<span class="legend">call status</span>'+
                         '</li>'+
                         '<li>'+
-                            '<span class="data">'+data[i].callstatus+'</span>'+
-                            '<span class="legend">call status</span>'+
-                        '</li>'+
-                        '<li>'+
-                            '<span class="data colored '+data[i].callType+'">'+ data[i].callType +'</span>'+
+                            '<span class="data colored '+value.callType+'">'+ value.callType +'</span>'+
                             '<span class="legend">callType</span>'+
                         '</li>'+
-                        // '<li>'+
-                        //     '<span class="data date '+monthNames[dateNow.getMonth()]+dateNow.getDate()+'">'+ checker +'</span>'+
-                        //     '<span class="legend">callType</span>'+
-                        // '</li>'+
                         '<li>'+
                             '<audio controls>'+
-                                '<source src="'+data[i].recordUrl+'" type="audio/mpeg">'+
+                                '<source src="'+value.recordUrl+'" type="audio/mpeg">'+
                             ' Your browser does not support the audio element.'+
                             ' </audio>'+
                         '</li>'+
                         '</ul>'+
                     '</div>';  
-                $(dataBox).appendTo('.'+divname);    
-                }        
-            }   
+                    $('.dashboard').append(dataBox);
+                }
 
-        })
-    }
-       
+
+            })
+
+            // for(var i in data){
+            //     var date = data[i].createdDate.split("T")[0];
+            //     var customernum = data[i].customerPhoneNumber;
+
+            //     if(customernum == mobile){
+            //         element = '<h3>Calls For'+date+'</h3>';
+            //         $('.dashboard').append(element);
+            //     }
+            //     dataBox = '<div class="divDetails">'+
+            //         '<ul>'+
+            //         '<li>'+
+            //             '<span class="data">'+data[i].customerPhoneNumber+'</span>'+
+            //             '<span class="legend">Phone Number</span>'+
+            //         '</li>'+
+            //         '<li>'+
+            //             '<span class="data">'+data[i].callstatus+'</span>'+
+            //             '<span class="legend">call status</span>'+
+            //         '</li>'+
+            //         '<li>'+
+            //             '<span class="data">'+data[i].callstatus+'</span>'+
+            //             '<span class="legend">call status</span>'+
+            //         '</li>'+
+            //         '<li>'+
+            //             '<span class="data colored '+data[i].callType+'">'+ data[i].callType +'</span>'+
+            //             '<span class="legend">callType</span>'+
+            //         '</li>'+
+            //         '<li>'+
+            //             '<audio controls>'+
+            //                 '<source src="'+data[i].recordUrl+'" type="audio/mpeg">'+
+            //             ' Your browser does not support the audio element.'+
+            //             ' </audio>'+
+            //         '</li>'+
+            //         '</ul>'+
+            //     '</div>';  
+            //     $(dataBox).appendTo('.dashboard');  
+        //     }
+        // })
+    })}
+    
     $('.mainContent ').delegate('.btnGet','click', function(){
         event.preventDefault();
         var selData = $(this).data('date')
